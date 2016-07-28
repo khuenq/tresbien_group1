ae68e58b03672b7a191733a6cbfc009d09c38c27
Trong Vinh <hoangtrongvinh1994@gmail.com>
2016-07-28 10:35:02 +0700

    hom nay em di i

--- app/code/local/SmartLab/Myblog/sql/myblog_setup/upgrade-0.1.2-0.1.3.php
+++ app/code/local/SmartLab/Myblog/sql/myblog_setup/upgrade-0.1.2-0.1.3.php
@@ -12,7 +12,7 @@ $collection = Mage::getModel('myblog/post')->getCollection()->addFieldToFilter('
 foreach ($collection as $item) {
     $id = $item->getId();
     $installer->run("
-        INSERT INTO `neotheme_customer_post` (`customer_id`, `post_id`) VALUES ('3', $id);
+        INSERT INTO `neotheme_customer_post` (`customer_id`, `post_id`) VALUES ('1', $id);
     ");
 }
 
